1cf89fbc5fb09b1a09cfe2e879f9beaa568f6d3a
lux-orc <xiaofeng.lu@orc.govt.nz>
2024-09-18 16:29:59 +1200

    Added more comment for the SQL scripts.

--- scripts/duckdb/2_run_after_pwsh_script_duckdb_limitations.sql
+++ scripts/duckdb/2_run_after_pwsh_script_duckdb_limitations.sql
@@ -1,10 +1,12 @@
 
--- Notes (limitations):
---     * Suggests use [uid] in the PowerShell script obtaining time series
---     * The 2nd column in each CSV file is following [Parameter]@[Plate]. Therefore,
---       NO way to request data on the same uid for different temporal resolutions.
---       i.e., the same [Parameter] and the same [Plate] but different temporal resolutions
---       will NOT work! Works when the [Parameter]s are different!
+/*
+Notes (limitations):
+    * Suggests use [uid] in the PowerShell script obtaining time series
+    * The 2nd column in each CSV file is following [Parameter]@[Plate]. Therefore,
+    NO way to request data on the same uid for different temporal resolutions.
+    i.e., the same [Parameter] and the same [Plate] but different temporal resolutions
+    will NOT work! Works when the [Parameter]s are different!
+*/
 
 
 -- duckdb
